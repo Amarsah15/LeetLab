ee10ec08e2856db1d7cc18b08954c5d4d0054211
Amarsah15 <gsah150803@hmail.com>
2025-07-15 22:27:29 +0530

    fix: update sameSite attribute value for JWT cookie in login function

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -92,7 +92,7 @@ export const login = async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
