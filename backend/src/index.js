c116332e90cbe883c915de401f97563883ec8b3a
Amarsah15 <gsah150803@hmail.com>
2025-07-15 20:43:41 +0530

    fix: Update CORS origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: process.env.VERCEL_URL || "http://localhost:5173",
+    origin:
+      "https://leetlab-60jrjsh8e-amarsah15s-projects.vercel.app/" ||
+      "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
