53f4eb56a4a6a5c1f862e2b2ae8169a59a2d4b9c
Amarsah15 <gsah150803@hmail.com>
2025-07-15 20:53:20 +0530

    fix: base URL for production environment in axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,17 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api/v1"
-      : "/api/v1",
+      : "https://leetlab-azp5.onrender.com/api/v1",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
