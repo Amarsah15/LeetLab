05cb7b1c0680e25cb958dcfa5eae52af7e1ba191
Amarsah15 <gsah150803@hmail.com>
2025-07-15 23:06:23 +0530

    fix: remove dynamic base URL for axios instance and set to production URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8000/api/v1"
-      : "https://leetlab-azp5.onrender.com/api/v1",
+  baseURL: "https://leetlab-azp5.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
